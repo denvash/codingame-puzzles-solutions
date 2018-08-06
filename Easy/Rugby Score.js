f5688e2efe076ed89bc95b7b5426fb88d56435f6
Dennis Vash <justdenva@gmail.com>
2018-08-07 02:01:46 +0300

    Add RugbyScore

---
+++ Easy/Rugby Score.js
@@ -0,0 +1,10 @@
+
+const N = parseInt(readline())
+
+for (let i = 0; i <= N / 5; ++i) {
+  for (let j = 0; i >= j; ++j) {
+    for (let k = 0; k <= N / 3; ++k) {
+      if (i * 5 + j * 2 + k * 3 == N) print([i, j, k].join(' '))
+    }
+  }
+}
